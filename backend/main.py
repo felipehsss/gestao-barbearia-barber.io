47adb842b73d0f6dc64a8bcaa6f59d70b901c106
Felipe <felipehssacchi@gmail.com>
2026-04-03 15:54:23 -0300

    nao sei

--- backend/main.py
+++ backend/main.py
@@ -2,6 +2,12 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+
 @app.get("/")
 def read_root():
-    return {"mensagem": "Olá, o backend em Python está a funcionar!"}
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: str | None = None):
+    return {"item_id": item_id, "q": q}
